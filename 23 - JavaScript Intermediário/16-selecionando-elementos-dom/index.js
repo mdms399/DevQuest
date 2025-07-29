e2429a888e02bfbcd71f42f651936eb0c4752430
Marcos Daniel <84116537+mdms399@users.noreply.github.com>
2025-07-29 20:33:06 -0300

    Manipulando o DOM

--- 23 - JavaScript Intermediário/16-selecionando-elementos-dom/index.js
+++ 23 - JavaScript Intermediário/16-selecionando-elementos-dom/index.js
@@ -1,1 +1,17 @@
 console.log(document)
+
+let p = document.getElementById('paragrafo1')
+
+console.log(p)
+
+let inputsPorClasse = document.getElementsByClassName('tag-de-input')
+
+console.log(inputsPorClasse[0])
+
+let inputsPorTag = document.getElementsByTagName('input');
+
+console.log(inputsPorTag[0]);
+
+let inputPorName = document.querySelectorAll('input[name="email"]');
+
+console.log(inputPorName);
