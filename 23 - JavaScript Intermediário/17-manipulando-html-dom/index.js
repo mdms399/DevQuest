b3ab5ea46624acf3cc0cbe0f2ce3c660a0caebc4
Marcos Daniel <84116537+mdms399@users.noreply.github.com>
2025-07-30 23:50:08 -0300

    Manipulando atributos e estulos dos elementos do DOM

--- 23 - JavaScript Intermediário/17-manipulando-html-dom/index.js
+++ 23 - JavaScript Intermediário/17-manipulando-html-dom/index.js
@@ -1,9 +1,13 @@
 function adicionarComentario() {
+    // Obtém o elemento de input pelo nome 'novo-comentario'
     let inputComentario = document.getElementsByName('novo-comentario');
 
+    // Pega o valor digitado no input (primeiro elemento do array retornado)
     let textoDigitado = inputComentario[0].value;
 
+    // Seleciona o elemento onde os novos comentários serão exibidos
     let novosComentarios = document.getElementById('novos-comentarios');
 
+    // Adiciona o novo comentário ao conteúdo HTML do elemento selecionado
     novosComentarios.innerHTML += `<p>Novo comentário: ${textoDigitado}</p>`;
 }
