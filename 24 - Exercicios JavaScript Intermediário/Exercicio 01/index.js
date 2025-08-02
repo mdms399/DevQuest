b62e3086197268be8f043d244fc562b29154e39a
Marcos Daniel <84116537+mdms399@users.noreply.github.com>
2025-08-01 21:24:01 -0300

    Exercicios, 1, 2 e 3

---
+++ 24 - Exercicios JavaScript Intermediário/Exercicio 01/index.js
@@ -0,0 +1,5 @@
+function trocarCor() {
+    let quadrados = document.getElementsByClassName('quadrado');
+    quadrados[0].classList.remove('cor1');
+    quadrados[0].classList.add('cor3');
+}
---
+++ 24 - Exercicios JavaScript Intermediário/Exercicio 02/index.js
@@ -0,0 +1,4 @@
+let numeros = [65, 44, 12, 4];
+numeros.forEach(function(numero) {
+    console.log(numero * 10);
+});
